ed05617197e612a38353f32fe7cee9f0de2ab599
JaonHax <36414836+JaonHax@users.noreply.github.com>
2020-07-27 09:41:27 -0500

    Add straight HTML scraper maybe

--- scpscraper/scpscraper.py
+++ scpscraper/scpscraper.py
@@ -383,3 +383,51 @@ def scrape_scps(min_skip: int=0, max_skip: int=6000, ai_dataset: bool=False):
     os.remove(f'{skip_file}.tmp')
 
   # print("Done!")
+
+def scrape_scps_html(min_skip: int=0, max_skip: int=6000):
+  # Create/reset text file
+  with open('scp_html.txt', "w"):
+    pass
+  
+  # Define blank page contents.
+  blank_page = '<div style="text-align: center;">\n<h1 id="toc0"><span>This page doesn\'t exist yet!</span></h1>\n</div>\n<hr>\n<div style="background-color: #600; border: solid 1px #600; border-radius: 20px; color: #fff; width: 450px; margin: 0 auto; font-size: 150%; text-align: center; box-shadow: 0 2px 6px rgba(0,0,0,.5), inset 0 1px rgba(255,255,255,.5), inset 0 10px rgba(255,204,204,.5), inset 0 10px 20px rgba(255,204,204,.3), inset 0 -15px 30px rgba(48,0,0,.5); line-height: 100%; padding: 0 10px;">\n<p><strong>Did you get feedback first?</strong></p>\n</div>\n<div style="background-color: #fff0f0; border: solid 1px #600; border-radius: 20px; color: #300; width: 450px; margin: 20px auto 0; text-align: center; box-shadow: 0 2px 6px rgba(0,0,0,.5); padding: 0 10px;">'
+  
+  for i in tqdm(range(min_skip, max_skip), "Fetching skips", total=max_skip, ncols=150, initial=min_skip+1, unit="skip", file=sys.stdout, bar_format='{desc}... {percentage:3.2f}% |{bar}|  [{remaining} remaining, {rate_fmt}]', smoothing=0.01875):
+    with open('scp_html.txt', "a") as out:
+      if i < 10:
+        j = f'00{i}'
+      elif i < 100:
+        j = f'0{i}'
+      else:
+        j = i
+      
+      soup = scpscraper.get_single_scp(j)
+      content = soup.find('div', id='page-content')
+      
+      if blank_page not in content:
+        out.write(f'{content}<\|endoftext\|>\n')
+      
+      else:
+        # print(f'\rThe page for SCP-{j} is blank!')
+        pass
+    
+    if i % 250 == 0 and i != 0:
+      try:
+        get_ipython().system('zip -9 -uq html_scraper_output.zip scp_html.txt')
+
+      except NameError:
+        try:
+          import zipfile
+          from sys import version_info
+
+          if version_info[0] == 3 and version_info[1] >= 7:
+            with zipfile.ZipFile('html_scraper_output.zip', "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zip_out:
+              zip_out.write('scp_html.txt')
+
+          elif version_info[0] == 3:
+            with zipfile.ZipFile('html_scraper_output.zip', "w", compression=zipfile.ZIP_DEFLATED) as zip_out:
+              zip_out.write('scp_html.txt')
+
+        except RuntimeError:
+          print('\rThe zlib module is needed to compress files into zip files!')
+          pass
