17e165d96f42c3e479bae181c945373dad6beb1e
JaonHax <36414836+JaonHax@users.noreply.github.com>
2020-07-27 11:54:29 -0500

    Seriously, how did I forget again?

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as fh:
 setup(
     name="scpscraper",
     packages=['scpscraper'],
-    version="0.1.2",
+    version="0.1.3b0",
     license="MIT",
     author="JaonHax",
     author_email="jaonhax@gmail.com",
