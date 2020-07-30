0b331010e3054d92cd55cea0f3470ca75a070b36
JaonHax <36414836+JaonHax@users.noreply.github.com>
2020-07-30 16:07:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as fh:
 setup(
     name="scpscraper",
     packages=['scpscraper'],
-    version="0.2.8a2",
+    version="0.2.8",
     license="MIT",
     author="JaonHax",
     author_email="jaonhax@gmail.com",
