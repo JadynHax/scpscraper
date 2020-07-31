15312f343d58cb4c513b975cad659ce6b85e6315
JaonHax <36414836+JaonHax@users.noreply.github.com>
2020-07-31 10:31:18 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as fh:
 setup(
     name="scpscraper",
     packages=['scpscraper'],
-    version="0.3.1",
+    version="0.3.2",
     license="MIT",
     author="JaonHax",
     author_email="jaonhax@gmail.com",
