769c5879709e78ea9ebc209bd7d7f794bc6f290b
JaonHax <36414836+JaonHax@users.noreply.github.com>
2020-07-31 16:57:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,26 +7,32 @@ with open("requirements.txt", "r") as fh:
   requirements = fh.readlines()
 
 setup(
-    name="scpscraper",
-    packages=['scpscraper'],
-    version="1.0.0",
-    license="MIT",
-    author="JaonHax",
-    author_email="jaonhax@gmail.com",
-    description="A Python library designed for scraping data from the SCP wiki.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/JaonHax/scp-scraper",
-    keywords=["scp", "foundation", "webscraper", "tensorflow", "dataset"],
-    install_requires=requirements,
-    include_package_data=True,
-    classifiers=[
-                 "Programming Language :: Python :: 3",
-                 "License :: OSI Approved :: MIT License",
-                 "Operating System :: OS Independent",
-                 "Development Status :: 3 - Alpha",
-                 "Intended Audience :: Developers",
-                 "Natural Language :: English",
-                 "Topic :: Scientific/Engineering :: Artificial Intelligence"
-    ]
+      name="scpscraper",
+      packages=['scpscraper'],
+      version="1.0.0",
+      license="MIT",
+      author="JaonHax",
+      author_email="jaonhax@gmail.com",
+      description="A Python library designed for scraping data from the SCP wiki.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/JaonHax/scp-scraper",
+      keywords=["scp", "foundation", "webscraper", "tensorflow", "dataset"],
+      install_requires=requirements,
+      include_package_data=True,
+      classifiers=[
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Programming Language :: Python :: 3.6",
+                   "Programming Language :: Python :: 3.7",
+                   "Programming Language :: Python :: 3.8",
+                   "Programming Language :: Python :: 3.9",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "Natural Language :: English",
+                   "Topic :: Scientific/Engineering :: Artificial Intelligence"
+      ],
+      python_requires='>=3.6'
 )
