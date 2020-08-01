aa84c1675db2c89d621b31209ad91889d91ab6bc
JaonHax <36414836+JaonHax@users.noreply.github.com>
2020-07-31 20:57:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r") as fh:
   requirements = fh.readlines()
 
 setup(
-      name="scpscraper",
+      name="scpscraper-dev",
       packages=['scpscraper'],
       version="1.0.0",
       license="MIT",
